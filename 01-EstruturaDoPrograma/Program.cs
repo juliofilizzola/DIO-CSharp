d9f7c3cacf6e1002d109cd1e59d71f00ef9e0bf3
Filizzola <juliofilizzola@hotmail.com>
2021-10-02 10:13:02 -0300

    organizando arquivos

---
+++ 01-EstruturaDoPrograma/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _01_EstruturaDoPrograma
+{
+    class Program
+    {
+        static void Main()
+        {
+            var s = new Pilha();
+            s.Empilha(1);
+            s.Empilha(10);
+            s.Empilha(100);
+            Console.WriteLine(s.Desempilha());
+        }
+    }
+}
