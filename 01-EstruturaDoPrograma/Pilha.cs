a9cb2243e83da351570600da3d18f7ebe83f2f41
Filizzola <juliofilizzola@hotmail.com>
2021-10-02 10:18:43 -0300

    criando Desempilha

--- 01-EstruturaDoPrograma/Pilha.cs
+++ 01-EstruturaDoPrograma/Pilha.cs
@@ -4,8 +4,12 @@ namespace _01_EstruturaDoPrograma {
   public class Pilha {
     Posicao primeiro;
 
-    public void Empilhar(object item) {
-      
+    public void Empilha(object item) {
+      primeiro = new Posicao(primeiro, item);
+    }
+
+    public object Desempilha() {
+
     }
   }
 }
