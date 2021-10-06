c93b77e5badb46a4cfbf31d75c91e6e6ebec4988
filizzola <juliofilizzola@hotmail.com>
2021-10-06 13:15:26 -0300

    criando função Declaracoes

--- 02-Instrucoes/Instrucoes/Instrucoes/Program.cs
+++ 02-Instrucoes/Instrucoes/Instrucoes/Program.cs
@@ -4,9 +4,25 @@ namespace Instrucoes
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] arg)
         {
-            
+            static void Declaracoes()
+            {
+                int a;
+                int b = 2, c = 3;
+                const int d = 4;
+                a = 1;
+                Console.WriteLine(a + b + c + d);
+            }
+            Declaracoes();
+            static void InstrucaoIf(string[] args) {
+
+            }
+            InstrucoesIF();
+            // static void InstrucaoSwitch(string[] args) { }
+            // static void InstrucaoWhile(string[] args) { }
+            // static void InstrucaoDo(string[] args) { }
+            // static void InstrucaoFor(string[] args) { }
 
         }
     }
